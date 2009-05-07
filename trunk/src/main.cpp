454ff113e28714085384944b3f22bcc0860439c3
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-05-07 23:38:33 +0000

    M    src/main.cpp M    src/BZFSCommunicator.cpp M    src/RobotController.cpp M    inc/RobotController.h M    inc/BZFSCommunicator.h M    Makefile

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -10,10 +10,83 @@
 
 using namespace std;
 
+
+void DisplayUsage();
+void DisplayBanner();
+bool ParseArgs(int argc, char** argv);
+
+char* arg0;
+bool DEBUG = false;
+string SERVER;
+string STR_PORT;
+int PORT;
+
+//------------------------------------------------------
 int main(int argc, char** argv) {
-    cout << "testing..." << endl;
-    RobotController controller;
+
+    DisplayBanner();
+
+    arg0 = argv[0];
+
+    if(!ParseArgs(argc, argv)) {
+        DisplayUsage();
+        exit(0);
+    }
+
+    RobotController controller("localhost", 3333);
 }   
+//------------------------------------------------------
+bool ParseArgs(int argc, char** argv) {
+    if(argc < 3 || argc > 4)
+        return false;
+    
+    string currArg = "";
+    for(int i = 1; i < argc; i++) {
+        currArg = argv[i];
+        if(currArg != "-d" && SERVER == "")
+            SERVER = currArg;
+        else if(currArg != "-d" && STR_PORT == "")
+            STR_PORT = currArg;
+        else if(currArg == "-d")
+            DEBUG = true;
+    }
+
+    //this isn't throwing an exception...
+    try {
+        PORT = atoi(STR_PORT.c_str());
+    } catch (...) {
+        cout << "The given port ("+STR_PORT+") is not a number." << endl
+             << "Go away and try again when you know what a number is." << endl;
+        return false;
+    }
+
+    if(DEBUG) {
+        cout << "DEBUG MODE:" << endl
+             << "    Server: " << SERVER << endl
+             << "    Port:   " << PORT << endl;
+    }
+    
+    return true;
+}
+//------------------------------------------------------
+void DisplayBanner() {
+    cout << endl
+         << "          +-------------------------------------------+" << endl
+         << "          |               AI for BZFLAG               |" << endl
+         << "          +-------------------------------------------+" << endl
+         << "                Nephi Johnson & William Armstrong" << endl
+         << "                             CS470" << endl
+         << endl;
+}
+//------------------------------------------------------
+void DisplayUsage() {
+    cout << "USAGE: " << arg0 << " [-d] <server> <port>" << endl
+         << endl
+         << "    -d       --    DEBUG MODE" << endl
+         << "    server   --    bzrobots server " << endl
+         << "    port     --    bzrobots port " << endl << endl << endl;
+}
+//------------------------------------------------------
 
 
 

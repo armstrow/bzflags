2422499783dc8264e17702388e842b583b144f1f
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-05-08 04:25:51 +0000

    M    src/BZFSCommunicator.cpp M    src/Robot.cpp M    src/PersonalData.cpp M    inc/PersonalData.h M    inc/Robot.h M    inc/BZFSCommunicator.h M    specs.txt M    Makefile

--- trunk/inc/PersonalData.h
+++ trunk/inc/PersonalData.h
@@ -2,17 +2,37 @@
 #define PERSONALDATA
 
 
+#include <iostream>
+
+using namespace std;
 
 
 class PersonalData {
-    
+ 
+    /*  +--------------+
+     *  |  VARIABLES   |
+     *  +--------------+ */
 
     public:
-
+        double xpos;
+        double ypox;
+        double angvel;
+        double speed;
 
     private:
 
+ 
 
+    /*  +--------------+
+     *  |  FUNCTIONS   |
+     *  +--------------+ */
+
+    public:
+        PersonalData();
+        void Parse(string data);
+
+    private:
+ 
 };
 
 
--- trunk/inc/Robot.h
+++ trunk/inc/Robot.h
@@ -2,16 +2,36 @@
 #define ROBOT
 
 
+#include "PersonalData.h"
+#include "BZFSCommunicator.h"
 
 class Robot {
 
+    /*  +--------------+
+     *  |  VARIABLES   |
+     *  +--------------+ */
 
     public:
 
+    private:
+        PersonalData personalData;
+        BZFSCommunicator *bzfsComm;
+
+
+
+    /*  +--------------+
+     *  |  FUNCTIONS   |
+     *  +--------------+ */
+
+    public:
+        Robot(BZFSCommunicator *bzfsComm);
+        void GuardBase(double aggression);
+        void AttackOthers(double aggression);
+        void GetFlag(double agression);
+        void SetPersonalData(string data);
 
     private:
-
-
+        void UpdatePosition();
 };
 
 

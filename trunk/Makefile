6f500d11df50b9d530adb1bf56d2dbb4250954a4
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-05-08 15:13:48 +0000

    M    src/EnvironmentData.cpp M    src/BZFSCommunicator.cpp M    src/RobotController.cpp M    inc/RobotController.h A    inc/EnvironmentData.h M    inc/BZFSCommunicator.h M    Makefile

--- trunk/Makefile
+++ trunk/Makefile
@@ -5,7 +5,7 @@
 CC= g++
 FLAGS= -Wall -ggdb -Iinc
 EXENAME= robotter
-OBJECTS= obj/RobotController.o obj/BZFSCommunicator.o obj/Robot.o obj/PersonalData.o
+OBJECTS= obj/RobotController.o obj/BZFSCommunicator.o obj/Robot.o obj/EnvironmentData.o obj/PersonalData.o obj/MyTank.o obj/OtherTank.o obj/Shot.o obj/Obstacle.o obj/Flag.o obj/Team.o obj/Constant.o
 
 
 
@@ -35,22 +35,43 @@ realclean: clean
 
 
 
-bin/$(EXENAME): src/main.cpp obj/RobotController.o
+bin/$(EXENAME): src/main.cpp $(OBJECTS)
 	$(CC) $(FLAGS) src/main.cpp -o bin/$(EXENAME) $(OBJECTS)
 
-obj/RobotController.o: inc/RobotController.h src/RobotController.cpp obj/BZFSCommunicator.o obj/Robot.o
+obj/RobotController.o: inc/RobotController.h src/RobotController.cpp obj/BZFSCommunicator.o obj/Robot.o obj/EnvironmentData.o
 	$(CC) $(FLAGS) -c src/RobotController.cpp -o obj/RobotController.o 
 
 obj/BZFSCommunicator.o: inc/BZFSCommunicator.h src/BZFSCommunicator.cpp
 	$(CC) $(FLAGS) -c src/BZFSCommunicator.cpp -o obj/BZFSCommunicator.o 
 
-obj/Robot.o: inc/Robot.h inc/Robot.h obj/BZFSCommunicator.o obj/PersonalData.o
+obj/Robot.o: inc/Robot.h inc/Robot.h obj/BZFSCommunicator.o obj/PersonalData.o obj/EnvironmentData.o
 	$(CC) $(FLAGS) -c src/Robot.cpp -o obj/Robot.o 
 
 obj/PersonalData.o: inc/PersonalData.h src/PersonalData.cpp
 	$(CC) $(FLAGS) -c src/PersonalData.cpp -o obj/PersonalData.o
 
+obj/EnvironmentData.o: inc/EnvironmentData.h src/EnvironmentData.cpp
+	$(CC) $(FLAGS) -c src/EnvironmentData.cpp -o obj/EnvironmentData.o
 
+obj/Team.o: inc/Team.h src/Team.cpp
+	$(CC) $(FLAGS) -c src/Team.cpp -o obj/Team.o
 
+obj/Flag.o: inc/Flag.h src/Flag.cpp
+	$(CC) $(FLAGS) -c src/Flag.cpp -o obj/Flag.o
+
+obj/Shot.o: inc/Shot.h src/Shot.cpp
+	$(CC) $(FLAGS) -c src/Shot.cpp -o obj/Shot.o
+
+obj/MyTank.o: inc/MyTank.h src/MyTank.cpp
+	$(CC) $(FLAGS) -c src/MyTank.cpp -o obj/MyTank.o
+
+obj/OtherTank.o: inc/OtherTank.h src/OtherTank.cpp
+	$(CC) $(FLAGS) -c src/OtherTank.cpp -o obj/OtherTank.o
+
+obj/Obstacle.o: inc/Obstacle.h src/Obstacle.cpp
+	$(CC) $(FLAGS) -c src/Obstacle.cpp -o obj/Obstacle.o
+
+obj/Constant.o: inc/Constant.h src/Constant.cpp
+	$(CC) $(FLAGS) -c src/Constant.cpp -o obj/Constant.o
 
 
--- trunk/src/RobotController.cpp
+++ trunk/src/RobotController.cpp
@@ -1,16 +1,46 @@
 
 
+#include "Team.h"
 #include "RobotController.h"
+
+#include <vector>
 
 
 //------------------------------------------------------
 RobotController::RobotController(string server, int port) {
     bzfsComm.Connect(server, port);
+
+    InitEnvironment();
+    LoopAction();
+
     //Start Dummy
     if (speed(0, 1))
-	cout << "Moving tank Success!!" << endl;
+	    cout << "Moving tank Success!!" << endl;
     else
-	cout << "Moving tank Failure =(" << endl;
+	    cout << "Moving tank Failure =(" << endl;
+}
+//------------------------------------------------------
+void RobotController::LoopAction() {
+    while(1 == 1) {
+        cout << "------------------------------------------" << endl;
+        cout << "------------------------------------------" << endl;
+        UpdateEnvironment();
+        vector<Team> teams;
+        bzfsComm.get_teams(&teams);
+        cout << "num teams: " << teams.size() << endl;
+        for(int i = 0; i < teams.size(); i++) {
+            cout << teams.at(i).ToString() << endl;
+        }
+        sleep(2);
+    }
+}
+//------------------------------------------------------
+void RobotController::InitEnvironment() {
+    bzfsComm.get_teams(&env.teams);
+}
+//------------------------------------------------------
+void RobotController::UpdateEnvironment() {
+    
 }
 //------------------------------------------------------
 
@@ -80,226 +110,4 @@ bool RobotController::accely(int index, double value) {
 	str_buff.append(char_buff);
 	return SendBoolMessage(str_buff);
 }
-
-/******************************
-* Information Requests
-******************************/
-bool RobotController::get_teams(){//vector <team_t> *AllTeams) {
-//Request a list of teams.
-	cout << bzfsComm.SendMessage("teams").at(0);
-/*
-	SendLine("teams");
-	ReadAck();
-	vector <string> v=ReadArr();
-	if(v.at(0)!="begin") {
-		return false;
-	}
-	v.clear();
-	v=ReadArr();
-	int i=0;
-	while(v.at(0)=="team") {
-		team_t MyTeam;
-		MyTeam.color=v.at(1);
-		MyTeam.count=atoi(v.at(2).c_str());
-		MyTeam.base_corner[0][0]=atof(v.at(3).c_str());
-		MyTeam.base_corner[0][1]=atof(v.at(4).c_str());
-		MyTeam.base_corner[1][0]=atof(v.at(5).c_str());
-		MyTeam.base_corner[1][1]=atof(v.at(6).c_str());
-		MyTeam.base_corner[2][0]=atof(v.at(7).c_str());
-		MyTeam.base_corner[2][1]=atof(v.at(8).c_str());
-		MyTeam.base_corner[3][0]=atof(v.at(9).c_str());
-		MyTeam.base_corner[3][1]=atof(v.at(10).c_str());
-		AllTeams->push_back(MyTeam);
-		v.clear();
-		v=ReadArr();
-		i++;
-	}
-	if(v.at(0)!="end") {
-		return false;
-	}
-	return true;
-}
-
-	bool get_obstacles(vector <obstacle_t> *AllObstacles) {
-		// Request a list of obstacles.
-		SendLine("obstacles");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="obstacle") {
-			obstacle_t MyObstacle;
-			int j=0;
-			while(j+2<(int)v.size()) {
-				MyObstacle.o_corner[j/2][0]=atof(v.at(j+1).c_str());
-				MyObstacle.o_corner[j/2][1]=atof(v.at(j+2).c_str());
-				j=j+2;
-			}
-			AllObstacles->push_back(MyObstacle);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			return false;
-		}
-		return true;
-	}
-
-	bool get_flags(vector <flag_t> *AllFlags) {
-		// Request a list of flags.
-		SendLine("flags");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="flag") {
-			flag_t MyFlag;
-			MyFlag.color=v.at(1);
-			MyFlag.poss_color=v.at(2);
-			MyFlag.pos[0]=atof(v.at(3).c_str());
-			MyFlag.pos[1]=atof(v.at(4).c_str());
-			AllFlags->push_back(MyFlag);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			return false;
-		}
-		return true;
-	}
-
-	bool get_shots(vector <shot_t> *AllShots) {
-		// Request a list of shots.
-		SendLine("shots");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="shot") {
-			shot_t MyShot;
-			MyShot.pos[0]=atof(v.at(1).c_str());
-			MyShot.pos[1]=atof(v.at(2).c_str());
-			MyShot.velocity[0]=atof(v.at(3).c_str());
-			MyShot.velocity[1]=atof(v.at(4).c_str());
-			AllShots->push_back(MyShot);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			return false;
-		}
-		return true;
-	}
-
-	bool get_mytanks(vector <tank_t> *AllMyTanks) {
-		// Request a list of our robots.
-		SendLine("mytanks");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="mytank") {
-			tank_t MyTank;
-			MyTank.index=atoi(v.at(1).c_str());
-			MyTank.callsign=v.at(2);
-			MyTank.status=v.at(3);
-			MyTank.shots_avail=atoi(v.at(4).c_str());
-			MyTank.time_to_reload=atof(v.at(5).c_str());
-			MyTank.flag=v.at(6);
-			MyTank.pos[0]=atof(v.at(7).c_str());
-			MyTank.pos[1]=atof(v.at(8).c_str());
-			MyTank.angle=atof(v.at(9).c_str());
-			MyTank.velocity[0]=atof(v.at(10).c_str());
-			MyTank.velocity[1]=atof(v.at(11).c_str());
-			MyTank.angvel=atof(v.at(12).c_str());
-			AllMyTanks->push_back(MyTank);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			if(debug) cout << v.at(0) << endl;
-			return false;
-		}
-		return true;
-	}
-
-	bool get_othertanks(vector <otank_t> *AllOtherTanks) {
-		// Request a list of tanks that aren't our robots.
-		SendLine("othertanks");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="othertank") {
-			otank_t OtherTank;
-			OtherTank.callsign=v.at(1);
-			OtherTank.color=v.at(2);
-			OtherTank.status=v.at(3);
-			OtherTank.flag=v.at(4);
-			OtherTank.pos[0]=atof(v.at(5).c_str());
-			OtherTank.pos[1]=atof(v.at(6).c_str());
-			OtherTank.angle=atof(v.at(7).c_str());
-			AllOtherTanks->push_back(OtherTank);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			return false;
-		}
-		return true;
-	}
-
-	bool get_constants(vector <constant_t> *AllConstants) {
-		// Request a dictionary of game constants.
-		SendLine("constants");
-		ReadAck();
-		vector <string> v=ReadArr();
-		if(v.at(0)!="begin") {
-			return false;
-		}
-		v.clear();
-		v=ReadArr();
-		int i=0;
-		while(v.at(0)=="constant") {
-			constant_t MyConstant;
-			MyConstant.name=v.at(1);
-			MyConstant.value=v.at(2);
-			AllConstants->push_back(MyConstant);
-			v.clear();
-			v=ReadArr();
-			i++;
-		}
-		if(v.at(0)!="end") {
-			return false;
-		}
-		return true;
-	}
-*/
-}
-
 

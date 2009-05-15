deec5398acf35a92fb200d48120ffe28c2d462d9
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-05-15 15:37:16 +0000

    M    BFSPlot.gpi M    src/IterativeDeepeningAlg.cpp M    src/DepthFirstAlg.cpp M    src/main.cpp

--- trunk/src/IterativeDeepeningAlg.cpp
+++ trunk/src/IterativeDeepeningAlg.cpp
@@ -7,6 +7,19 @@
 #include "SearchAlg.h"
 #include "GnuplotWriter.h"
 
+#define BLACK  -1
+#define RED     1
+#define GREEN   2
+#define BLUE    3
+#define MAGENTA 4
+#define AQUA    5
+#define BROWN   6
+#define YELLOW  7
+#define ORANGE  8
+
+#define DELAY   1
+
+
 
 
 

d8c4ea69c6579d3406edd3d86a392fa79a860e7f
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-05-16 15:08:37 +0000

    M    src/IterativeDeepeningAlg.cpp M    src/main.cpp M    BFSPlot.gpi M    inc/IterativeDeepeningAlg.h

--- src/IterativeDeepeningAlg.cpp
+++ src/IterativeDeepeningAlg.cpp
@@ -47,9 +47,10 @@ string IterativeDeepeningAlg::DoSearch(Position startNode, Position endNode) {
 
     int maxDepth = 1;
 
-    while ((pos.row != endNode.row || pos.col != endNode.col)) {
-        if(pos.level <= maxDepth){
-            s += EnqueueNeighbors(pos);
+    Position *currPos = &pos;
+    while ((currPos->row != endNode.row || currPos->col != endNode.col)) {
+        if(currPos->level <= maxDepth){
+            s += EnqueueNeighbors(currPos);
         }
 
         if(q.empty()) {
@@ -62,8 +63,7 @@ string IterativeDeepeningAlg::DoSearch(Position startNode, Position endNode) {
                     map->at(i).at(j)->visited = false;
 
             maxDepth += 1;
-            pos.set(startNode.row, startNode.col);
-            pos.level = 0;
+            currPos = &pos;
             continue;
         }
 
@@ -72,13 +72,20 @@ string IterativeDeepeningAlg::DoSearch(Position startNode, Position endNode) {
             return s;
         }
 
-        Position tmp = (Position)q.top();
-        s += gw->PrintLine(map->at(pos.row).at(pos.col), map->at(tmp.row).at(tmp.col), ORANGE);
+        Position *tmp = q.top();
+        s += gw->PrintLine(map->at(currPos->row).at(currPos->col), map->at(tmp->row).at(tmp->col), ORANGE);
         s += gw->PrintAniData(DELAY);
-        pos.set(tmp.row, tmp.col);
-        pos.level = tmp.level;
+        currPos = tmp;
         q.pop();
-        cout << "checking node at: " << pos.row << "," << pos.col << endl;
+        cout << "checking node at: " << currPos->row << "," << currPos->col << endl;
+    }
+
+    Position *lastPos = currPos;
+    while(lastPos->col != startNode.col || lastPos->row != startNode.row) {
+        cout << "in loop" << endl;
+        s += gw->PrintLine(map->at(lastPos->row).at(lastPos->col), map->at(lastPos->from->row).at(lastPos->from->col), GREEN);
+        s += gw->PrintAniData(0);
+        lastPos = lastPos->from;
     }
 
     return s;
@@ -88,31 +95,32 @@ vector<Node *> IterativeDeepeningAlg::GetBestPath() {
     
 }
 //------------------------------------------------------
-string IterativeDeepeningAlg::EnqueueNeighbors(Position p) {
+string IterativeDeepeningAlg::EnqueueNeighbors(Position *p) {
     string s = "";
-    s += EnQ(p.row - 1, p.col - 1, p);
-    s += EnQ(p.row - 1, p.col, p);
-    s += EnQ(p.row - 1, p.col + 1, p);
-    s += EnQ(p.row, p.col + 1, p);
-    s += EnQ(p.row + 1, p.col + 1, p);
-    s += EnQ(p.row + 1, p.col, p);
-    s += EnQ(p.row + 1, p.col - 1, p);
-    s += EnQ(p.row, p.col - 1, p);
+    s += EnQ(p->row - 1, p->col - 1, p);
+    s += EnQ(p->row - 1, p->col, p);
+    s += EnQ(p->row - 1, p->col + 1, p);
+    s += EnQ(p->row, p->col + 1, p);
+    s += EnQ(p->row + 1, p->col + 1, p);
+    s += EnQ(p->row + 1, p->col, p);
+    s += EnQ(p->row + 1, p->col - 1, p);
+    s += EnQ(p->row, p->col - 1, p);
     return s;
 }
 //------------------------------------------------------
-string IterativeDeepeningAlg::EnQ(int row, int col, Position from) {
+string IterativeDeepeningAlg::EnQ(int row, int col, Position *from) {
     string s = ""; 
     //bounds check
     if (row >= map->size() || col >= map->size() || row < 0 || col < 0)
         return s;
     if (map->at(row).at(col)->visitable && !map->at(row).at(col)->visited) {
         map->at(row).at(col)->visited = true;
-        Position n(row, col);
-        n.level = from.level + 1;
+        Position *n = new Position(row, col);
+        n->level = from->level + 1;
+        n->from = from;
         q.push(n);
         s += gw->PrintNode(map->at(row).at(col), BROWN);
-        s += gw->PrintLine(map->at(from.row).at(from.col), map->at(row).at(col), BLACK);
+        s += gw->PrintLine(map->at(from->row).at(from->col), map->at(row).at(col), BLACK);
         s += gw->PrintAniData(DELAY);
     }
     return s;

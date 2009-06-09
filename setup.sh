22bdc41398791d4ca3c1a603c160b0ab6646c54f
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-06-09 01:06:33 +0000

    M    src/KalmenFilter.cpp M    src/BZFSCommunicator.cpp M    src/Robot.cpp M    empty.bzw M    unvisitable.gpi M    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-gnome-terminal -x ~/bzflag/src/bzfs/bzfs -c -d -noTeamKills -set _inertiaLinear 1 -set _inertiaAngular 1 -set _tankAngVel 0.5 -set _explodeTime 0 -set _rejoinTime 0 -set _grabOwnFlag 0 -world empty.bzw &
+gnome-terminal -x ~cs470s/bzflag/src/bzfs/bzfs -c -d -noTeamKills -set _inertiaLinear 1 -set _inertiaAngular 1 -set _tankAngVel 0.5 -set _explodeTime 0 -set _rejoinTime 0 -set _grabOwnFlag 0 -world empty.bzw -ms 2 -worldsize 200 &
 read -p "Press any key to continue (when the bzfs is setup)"
-gnome-terminal -x ~/bzflag/src/bzrobots/bzrobots -team green -solo 1 -p 6000 &
-gnome-terminal -x ~/bzflag/src/bzrobots/bzrobots -team red -solo 1 -p 6005 badguys@mek.com &
+gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team green -solo 1 -p 6000 &
+gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team red -solo 1 -p 6005 -posnoise 0 badguys@mek.com &
 
 
 

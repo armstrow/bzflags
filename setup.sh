60bcc7293bcfce9386c7132df38c53f29ff23d1c
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-06-10 22:06:15 +0000

    D    opengui.sh M    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+~cs470s/bzflag/src/bzflag/bzflag -directory /users/ta/cs470s/bzflag/data -window 1024x768 &
 gnome-terminal -x ~cs470s/bzflag/src/bzfs/bzfs -c -d -noTeamKills -set _inertiaLinear 1 -set _inertiaAngular 1 -set _tankAngVel 0.5 -set _explodeTime 0 -set _rejoinTime 0 -set _grabOwnFlag 0 -world empty2.bzw -ms 2 -worldsize 400 &
 read -p "Press any key to continue (when the bzfs is setup)"
 gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team green -solo 1 -p 6000 &

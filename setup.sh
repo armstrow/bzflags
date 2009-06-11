3131fb1fecd1aaabb541226302a6b4a134360fec
nephi.johnson <nephi.johnson@0683b0e6-36af-11de-8ef0-0f53c1b7e458>
2009-06-11 21:45:55 +0000

    A    pacman.bzw M    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 gnome-terminal -x ~cs470s/bzflag/src/bzfs/bzfs -c -d -noTeamKills -set _inertiaLinear 1 -set _inertiaAngular 1 -set _tankAngVel 0.5 -set _explodeTime 0 -set _rejoinTime 0 -set _grabOwnFlag 0 -world empty2.bzw -ms 3 -worldsize 200 &
 read -p "Press any key to continue (when the bzfs is setup)"
 gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team green -solo 1 -p 6000 &
-gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team red -solo 4 -p 6005 -posnoise 3 badguys@mek.com &
+gnome-terminal -x ~cs470s/bzflag/src/bzrobots/bzrobots -team red -solo 10 -p 6005 -posnoise 3 badguys@mek.com &
 
 
 
